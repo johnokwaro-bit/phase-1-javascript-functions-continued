966cef4b070632325fb615ecad3934070995cd66
John Okwaro <john.okwaro@moringaschool.com>
2022-08-19 03:49:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(monActivity = "go to the office"){
+    return `This Monday, I will ${monActivity}.`
+}
+
+function wrapAdjective(adjective = "*"){
+    const innerFunction = function(parameter = "special"){
+        return `You are ${adjective}${parameter}${adjective}!`
+    }
+    return innerFunction
+}
